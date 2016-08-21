a09f3dfcc166eaea92654b937c6a8935188bc8a1
TheIllusion <gunboy83@gmail.com>
2016-08-21 09:41:30 +0900

    Adaptive HistoEQ.

--- produce_adaptive_histoeq_images.py
+++ produce_adaptive_histoeq_images.py
@@ -11,7 +11,7 @@ from matplotlib import pyplot as plt
 #save_directory = '/Users/Illusion/Documents/Data/palm_data/NHN_palms/background_subtracted_skin_tone/histogram_equalized_images/'
 
 source_image_dir = '/Users/Illusion/Temp/jpgs'
-save_directory = '/Users/Illusion/Temp/jpgs/histogram_equalized_images/'
+save_directory = '/Users/Illusion/Temp/jpgs/adaptive_histoeq_images/'
 
 os.chdir(source_image_dir)
 
@@ -25,9 +25,11 @@ for filename in image_files:
     if match:
         img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
 
-        equalized_img = cv2.equalizeHist(img)
+        # create a CLAHE object (Arguments are optional).
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+        equalized_img = clahe.apply(img)
 
-        new_filename = 'histo_equalized_' + filename
+        new_filename = 'adap_histoeq_' + filename
 
         cv2.imwrite(save_directory + new_filename, equalized_img)
 
